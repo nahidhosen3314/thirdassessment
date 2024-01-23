0b1e8da50988d9e31d225862f62cd821d39a7af7
unknown <nahidhosen3314@gmail.com>
2024-01-23 20:31:49 +0600

    secend commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,5 @@
 
-$(function(){
-
+$(function(){ 
     
     // Menu
     $(".h-menu").clone().appendTo(".mobile-menu");
@@ -24,72 +23,7 @@ $(function(){
         updateHeight();
     });
 
-    
 
-
-    // course-card
-    $(".course-card").owlCarousel({
-        margin: 20,
-        dots: false,
-        nav: true,
-        autoplay: true,
-        loop: true,
-        smartSpeed: 1000,
-        responsiveClass:true,
-        responsive: {
-            0:{
-                items: 1,
-                nav: false,
-                dots: true,
-            },
-            700:{
-                items: 2,
-
-            },
-            1050:{
-                items: 3,
-            }
-        }
-    });
-
-    // Testimonial
-    var testimonial = $(".students").owlCarousel({
-        items:1,
-        smartSpeed: 1000,
-        autoplay: true,
-        loop: true,
-        dots: false,
-        items: 1,
-
-    });
-
-    $('.customNextBtn').click(function() {
-        testimonial.trigger('next.owl.carousel');
-    })
-    $('.customPrevBtn').click(function() {
-        testimonial.trigger('prev.owl.carousel', [1000]);
-    })
-
-    // logo
-
-    $(".logo").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1000,
-        margin: 20,
-        loop: true,
-        responsive: {
-            0:{
-                items: 1,
-            },
-            700:{
-                items: 3,
-
-            },
-            1050:{
-                items: 5,
-            }
-        }
-    })
     // author
 
     $(".author").owlCarousel({
